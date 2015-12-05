34d2707dfa9acb71d95b36faecc4a70b251bfd03
Andrew Mason <andrew@fixedpoint.xyz>
2015-12-05 07:19:38 -0800

    i was not aware there were "part 2"s

--- day1/main.rs
+++ day1/main.rs
@@ -1,15 +1,45 @@
-#![feature(iter_arith)]
-
 use std::env::args;
+
+struct Santa {
+    position: i32,
+    first_time_in_basement: i32,
+    num_movements: i32,
+}
+
+impl Santa {
+    fn has_been_in_basement(&self) -> bool {
+        self.first_time_in_basement >= 0
+    }
+
+    fn from_santa(santa: Santa, movement: i32) -> Santa {
+        let pos = santa.position + movement;
+        let movements = santa.num_movements + 1;
+        let basement = if santa.has_been_in_basement() || pos >= 0 {
+            santa.first_time_in_basement
+        } else {
+            movements
+        };
+        Santa {
+            position: pos,
+            first_time_in_basement: basement,
+            num_movements: movements,
+        }
+    }
+}
 
 fn main() {
     let instructions = match args().nth(1) {
         Some(v) => v,
         None => "".to_string(),
     };
-    let floor: i32 = instructions
+    let santa = Santa {
+        position: 0,
+        first_time_in_basement: -1,
+        num_movements: 0,
+    };
+    let last_santa: Santa = instructions
         .chars()
         .map(|c: char| if c == '(' { 1 } else { -1 })
-        .sum();
-    println!("{}", floor);
+        .fold(santa, |s, i| Santa::from_santa(s, i));
+    println!("{} {}", last_santa.position, last_santa.first_time_in_basement);
 }
