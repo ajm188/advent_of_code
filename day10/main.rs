01963af4ab4246b874b4ff80f9cc3801d3def21a
Andrew Mason <andrew@fixedpoint.xyz>
2015-12-18 09:48:13 -0800

    day 10, part 2

--- day10/main.rs
+++ day10/main.rs
@@ -1,17 +1,4 @@
-/*
-fn seq_counts<T: Eq + PartialEq + Clone>(list: Vec<T>) -> Vec<(T, usize)> {
-    let len = list.len();
-    let mut vec = vec![];
-    let mut items = 0;
-    while items < len {
-        let first = list.iter().skip(items).next().unwrap();
-        let count = list.iter().skip(items).take_while(|&x| first == x).count();
-        vec.push((first.clone(), count));
-        items += count;
-    }
-    vec
-}
-*/
+use std::env::args;
 
 fn seq_counts(list: Vec<char>) -> Vec<(char, usize)> {
     let mut vec = vec![];
@@ -30,9 +17,10 @@ fn concat(substrings: Vec<String>) -> String {
 }
 
 fn main() {
+    let iters_arg = args().skip(1).next().unwrap_or("40".to_string());
+    let iters = u32::from_str_radix(&iters_arg, 10).unwrap();
     let chars: Vec<char> = "3113322113".chars().collect();
-    //let chars: Vec<char> = "1".chars().collect();
-    let result = (0..40).fold(
+    let result = (0..iters).fold(
         chars,
         |s, _| {
             let subseq: Vec<String> = seq_counts(s).iter().map(|tup| {
@@ -45,10 +33,4 @@ fn main() {
         }
     );
     println!("{}", result.len());
-    /*
-    for c in result {
-        print!("{}", c);
-    }
-    println!("");
-    */
 }
