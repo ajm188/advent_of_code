f283599d0d95f945352f5a6dc51a72b4ac2c6f12
Andrew Mason <andrew@fixedpoint.xyz>
2015-12-26 07:24:04 -0800

    day 17, part 1

---
+++ day17/main.rs
@@ -0,0 +1,40 @@
+#![feature(iter_arith)]
+use std::env::args;
+
+fn powerset<T: Clone>(t: Vec<T>) -> Vec<Vec<T>> {
+    pset(t, vec![vec![]])
+}
+
+fn pset<T: Clone>(t: Vec<T>, s: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    if t.len() == 0 {
+        s
+    } else {
+        let head = t.first().unwrap();
+        let tail = t.iter().skip(1).map(|a| a.clone()).collect();
+        let mut ps: Vec<Vec<T>> = s.iter().map(|v| {
+            let mut vec = v.clone();
+            vec.push(head.clone());
+            vec
+        }).collect();
+        ps.extend(s);
+        pset(tail, ps)
+    }
+}
+
+fn main() {
+    let eggnog = 150;
+    let containers =
+        args()
+        .skip(1)
+        .map(|arg| u32::from_str_radix(&arg, 10).unwrap())
+        .collect();
+    let arrangements = 
+        powerset(containers)
+        .iter()
+        .filter(|v| {
+            let total: u32 = v.iter().sum();
+            total == eggnog
+        })
+        .count();
+    println!("{}", arrangements);
+}
