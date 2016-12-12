eb50b4cfcca01bc3131cbd23dcd6ff92c71cd800
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-12 17:14:43 -0500

    And there's part 2! I did good abstraction on this one

--- 2016/12/Main.hs
+++ 2016/12/Main.hs
@@ -2,8 +2,11 @@ import CPU
 import Instruction
 
 startCPU = (0, (0, 0, 0, 0)) :: CPU
+startCPUPart2 = (0, (0, 0, 1, 0)) :: CPU
 
 main = do
     input <- getContents
     print $
-        lookupReg (eval (map read $ lines input) startCPU) "a"
+        ( lookupReg (eval (map read $ lines input) startCPU) "a"
+        , lookupReg (eval (map read $ lines input) startCPUPart2) "a"
+        )
