af0c43b3ff7fde9ec71de6f676950a8ba0f9f14d
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-13 20:05:35 -0500

    Day 6, part 2

--- 2016/06/Main.hs
+++ 2016/06/Main.hs
@@ -3,8 +3,14 @@ import Data.List
 decode :: [String] -> String
 decode columns = foldl (++) "" $ map (\x -> [mostFrequent x]) columns
 
+decode2 :: [String] -> String
+decode2 columns = foldl (++) "" $ map (\x -> [leastFrequent x]) columns
+
 mostFrequent :: Eq a => [a] -> a
 mostFrequent items = fst $ maxBy (\(_, x) (_, y) -> compare x y) $ countDupes items
+
+leastFrequent :: Eq a => [a] -> a
+leastFrequent items = fst $ minBy (\(_, x) (_, y) -> compare x y) $ countDupes items
 
 countDupes :: Eq a => [a] -> [(a, Int)]
 countDupes items = foldl counter [] items
@@ -17,7 +23,14 @@ maxBy f (x:xs) = foldl orderer x xs
     where orderer x y = case f x y of LT -> y
                                       _  -> x
 
+minBy :: (a -> a -> Ordering) -> [a] -> a
+minBy f (x:xs) = foldl orderer x xs
+    where orderer x y = case f x y of GT -> y
+                                      _  -> x
+
 main = do
     input <- getContents
     print $
         decode $ transpose $ lines input
+    print $
+        decode2 $ transpose $ lines input
