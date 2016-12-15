2c69e64a00387c8063cf1b0ed05e67c4b919c801
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-15 10:39:47 -0500

    Day 3, part 2

--- 2016/03/Main.hs
+++ 2016/03/Main.hs
@@ -1,3 +1,5 @@
+import Data.List (transpose)
+
 isTriangle :: (Int, Int, Int) -> Bool
 isTriangle (a, b, c) = a + b > c && b + c > a && a + c > b
 
@@ -7,7 +9,15 @@ toInts line = map (\x -> read x :: Int) $ words line
 asTriplet :: [Int] -> (Int, Int, Int)
 asTriplet (a:b:c:_) = (a, b, c)
 
+groupBy :: Int -> [String] -> [[String]]
+groupBy x [] = []
+groupBy x items = (transpose $ map words $ take x $ items) ++ groupBy x (drop x items)
+
 main = do
     input <- getContents
     print $ length $ filter isTriangle $
         (map (asTriplet . toInts) $ lines input)
+    print $
+        length $ filter isTriangle $
+        map (asTriplet . toInts . unwords) $
+        groupBy 3 $ lines input
