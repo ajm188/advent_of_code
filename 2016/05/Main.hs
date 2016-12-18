335969d9f27e83809afceb7633213e3347f647d6
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-18 17:58:48 -0500

    Day 5, part 2

--- 2016/05/Main.hs
+++ 2016/05/Main.hs
@@ -10,5 +10,23 @@ password door len = (fst . (until ((\(p, _) -> length p == len)) next)) ("", 0)
           isInPassword = ((=="00000") . (take 5))
           hash' = (hash door)
 
+password2 door = ((map fst) . fst . (until (\(pass', _) -> foundPassword2 pass') next)) (zip ['0'..'7'] (repeat False), 0)
+    where next (pass', i) = if (isInPassword . hash') i then ((updatePassword2 pass') (hash' i), i + 1) else (pass', i + 1)
+          isInPassword = ((=="00000") . (take 5))
+          hash' = (hash door)
+
+updatePassword2 :: [(Char, Bool)] -> String -> [(Char, Bool)]
+updatePassword2 pass' h
+    | validPosition = updatePassword2'
+    | otherwise = pass'
+    where posChar = h !! 5
+          pos = (read . (:"")) posChar
+          validPosition = elem posChar ['0'..'7'] && (not . snd) (pass' !! pos)
+          updatePassword2' = take pos pass' ++ [(h !! 6, True)] ++ drop (pos + 1) pass'
+
+foundPassword2 :: ([(Char, Bool)] -> Bool)
+foundPassword2 = (all id) . (map snd)
+
 main = do
     print $ password doorId 8
+    print $ password2 doorId
