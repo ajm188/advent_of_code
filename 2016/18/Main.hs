0c3471235d43b6d201b69cd2a6dacdc9bc063ed0
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-18 08:45:15 -0500

    Day 18, part 1

---
+++ 2016/18/Main.hs
@@ -0,0 +1,28 @@
+isTrap :: (Bool, Bool, Bool) -> Bool
+isTrap (left, center, right)
+    | left && center && not right = True
+    | center && right && not left = True
+    | left && (not center && not right) = True
+    | right && (not center && not left) = True
+    | otherwise = False
+
+toTrap :: Char -> Bool
+toTrap '.' = False
+toTrap '^' = True
+
+nextRow :: [Bool] -> [Bool]
+nextRow row = map isTrap (zip3 (False : (take ((length row) - 1) row)) row (tail (row ++ [False])))
+
+room :: Int -> [[Bool]] -> [[Bool]]
+room x rows
+    | length rows == x = rows
+    | otherwise = room x (((nextRow . head) rows) : rows)
+
+main = do
+    input <- getContents
+    let startRow = map toTrap ((head . lines) input)
+    let numRows = ((read . head . tail . lines) input) :: Int
+    print $
+        sum $
+        map (length . (filter not)) $
+        room numRows [startRow]
