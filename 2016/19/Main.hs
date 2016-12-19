6bc937ae2c65f88d7747003c82fff7083d176dff
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-19 15:08:07 -0500

    Day 19, part 2

--- 2016/19/Main.hs
+++ 2016/19/Main.hs
@@ -5,7 +5,22 @@ next xs
     | (even . length) xs = ((map fst) . (filter (\(a, b) -> even b))) (zip xs [0..])
     | otherwise = (last xs) : (next ((reverse . tail . reverse) xs))
 
+next2 :: [a] -> [a]
+next2 [] = []
+next2 (x:[]) = [x]
+next2 xs = (drop numRemoved front) ++ back' ++ (take numRemoved front)
+    where half = (div (length xs) 2)
+          (front, back) = splitAt half xs
+          back'
+            | length back < 3 = tail back
+            | otherwise = ((map fst) . (filter (\(_, b) -> (mod b 3) == 0))) (zip back [indexStart..])
+          indexStart = if (even . length) xs then 1 else 2
+          numRemoved = (length back) - (length back')
+
 winner elves = until (done) (next) elves
+    where done = (==1) . length
+
+winner2 elves = until done next2 elves
     where done = (==1) . length
 
 main = do
@@ -13,3 +28,6 @@ main = do
     print $
         head $
         winner [1..num]
+    print $
+        head $
+        winner2 [1..num]
