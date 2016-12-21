0d48f5b03afd8ebda2a535f787a7e6b9a5c018f5
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-20 20:24:00 -0500

    Day 20, part 2; plus improvements to part 1 :smile:

--- 2016/20/Main.hs
+++ 2016/20/Main.hs
@@ -1,11 +1,26 @@
+import Data.Ix
+
 allIPs = [0..4294967295]
 
-inRange :: Ord a => a -> (a, a) -> Bool
-inRange x (y, z) = x >= y && x <= z
+rangeOverlaps :: Ix a => (a, a) -> (a, a) -> Bool
+rangeOverlaps r1@(low, high) r2@(low', high') = (any (inRange r2) [low, high]) || (any (inRange r1) [low', high'])
+
+mergeRanges :: Ix a => [(a, a)] -> [(a, a)]
+mergeRanges rs = fst (until done next (rs, []))
+    where done (a, b) = a == b
+          next ([], _) = ([], [])
+          next (r'@(r:rs), _) = ((foldl next' r (filter (rangeOverlaps r) rs)) : (mergeRanges (filter (not . (rangeOverlaps r)) rs)), r')
+          next' (low, high) (low', high') = (minimum [low, low'], maximum [high, high'])
+
+numAllowedIPs :: [Int] -> [String] -> Int
+numAllowedIPs allIPs lines = maxIP - totalBlocked + 1
+    where blocked = (mergeRanges . blockedIPs) lines
+          totalBlocked = (sum . (map (\(low, high) -> (high - low + 1)))) blocked
 
 lowestAllowedIP :: [Int] -> [String] -> Int
-lowestAllowedIP allIPs lines = (head . (filter (\x -> all (not . (inRange x)) blocked))) allIPs
+lowestAllowedIP allIPs lines = (head . (filter (\x -> all (not . (inRange' x)) blocked))) allIPs
     where blocked = blockedIPs lines
+          inRange' x r = inRange r x
 
 blockedIPs :: [String] -> [(Int, Int)]
 blockedIPs [] = []
@@ -20,4 +35,7 @@ main = do
     input <- getContents
     print $
         lowestAllowedIP allIPs $
+        lines input
+    print $
+        numAllowedIPs allIPs $
         lines input
