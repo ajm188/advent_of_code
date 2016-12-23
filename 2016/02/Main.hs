f9963a63bb68d5abf82741b19c42f56af4e34e96
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-23 06:25:00 -0500

    Adds generic passcode function

--- 2016/02/Main.hs
+++ 2016/02/Main.hs
@@ -1,12 +1,14 @@
 import Direction
 import Move
 
+passcode :: ((Char -> String -> Char) -> [String] -> String)
+passcode buttonFunc = (tail . reverse . (foldl (\code line -> (buttonFunc (head code) line) : code) ['5']))
+
 nextButton :: Char -> String -> Char
 nextButton button directions = foldl Move.move button $ map charToDirection directions
 
 main = do
     input <- getContents
     print $
-        tail $
-        reverse $
-        foldl (\code line -> (nextButton (head code) line) : code) ['5'] (lines input)
+        passcode nextButton $
+        lines input
