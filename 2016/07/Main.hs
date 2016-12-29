2fcc8dec90ac65e6aecf5fb8f2090d94ecd048ce
Andrew Mason <andrew@fixedpoint.xyz>
2016-12-29 13:55:43 -0500

    Day 7, part 2

--- 2016/07/Main.hs
+++ 2016/07/Main.hs
@@ -9,6 +9,36 @@ hasTLS' (a:b:b':a':xs) bracketDepth foundAbba
     | otherwise = hasTLS' rest bracketDepth foundAbba
     where rest = (b:b':a':xs)
 
+hasSSL :: String -> Bool
+hasSSL ip = any hasBAB hypernets
+    where (supernets, hypernets) = decompose ip
+          abas = foldl (++) [] (map findABAs supernets)
+          hasBAB hypernet = any (\(a:b:_) -> containsSequence [b, a, b] hypernet) abas
+
+decompose :: String -> ([String], [String])
+decompose [] = ([], [])
+decompose ('[':xs) = (supernets, inside : hypernets)
+    where inside = takeWhile (/=']') xs
+          rest = drop ((length inside) + 1) xs
+          (supernets, hypernets) = decompose rest
+decompose ip@(x:xs) = (supernet : supernets, hypernets)
+    where supernet = takeWhile (/='[') ip
+          rest = drop (length supernet) ip
+          (supernets, hypernets) = decompose rest
+
+findABAs :: Eq a => [a] -> [[a]]
+findABAs xs
+    | length xs < 3 = []
+    | (head xs) == (head (drop 2 xs)) && (head xs) /= (head (tail xs)) = (take 3 xs) : (findABAs (tail xs))
+    | otherwise = findABAs (tail xs)
+
+containsSequence :: Eq a => [a] -> [a] -> Bool
+containsSequence seq xs
+    | length xs < length seq = False
+    | (take (length seq) xs) == seq = True
+    | otherwise = containsSequence seq (tail xs)
+
 main = do
     input <- getContents
     print $ length $ filter hasTLS $ lines input
+    print $ length $ filter hasSSL $ lines input
