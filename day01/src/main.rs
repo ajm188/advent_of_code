54ce607ead87428b0d3c97a9f515ec43b86d0806
Andrew Mason <amason@slack-corp.com>
2019-12-24 07:46:18 -0500

    Add part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,12 +2,24 @@ use std::env::args;
 
 fn fuel_required(mass: i32) -> i32 { (mass / 3) - 2 }
 
+fn full_fuel_required(mass: i32) -> i32 {
+    let mut fuel = fuel_required(mass);
+    let mut new_fuel = fuel_required(fuel);
+    while new_fuel > 0 {
+        fuel += new_fuel;
+        new_fuel = fuel_required(new_fuel);
+    }
+
+    fuel
+}
+
 fn main() {
-    let modules = args().skip(1).map(|arg| {
+    let modules: Vec<i32> = args().skip(1).map(|arg| {
         arg.parse().unwrap()
-    });
+    }).collect();
 
-    let total_fuel: i32 = modules.map(|module| { fuel_required(module) }).sum();
+    let total_fuel: i32 = modules.iter().map(|module| { fuel_required(*module) }).sum();
+    let full_total_fuel: i32 = modules.iter().map(|module| { full_fuel_required(*module) }).sum();
 
-    println!("{}", total_fuel);
+    println!("{}\n{}", total_fuel, full_total_fuel);
 }
