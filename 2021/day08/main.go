2894c02440c67764d6b971296f870709c6393a6f
Andrew Mason <amason@hey.com>
2021-12-08 17:21:40 -0500

    day8, part2 gross-but-fun!!!

--- 2021/day08/main.go
+++ 2021/day08/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/ajm188/advent_of_code/pkg/cli"
+	"github.com/ajm188/advent_of_code/pkg/text"
 )
 
 var (
@@ -21,10 +23,61 @@ var (
 		8: "abcdefg",
 		9: "abcdfg",
 	}
+	segmentsToNumbers       map[string]int
 	uniqueSegmentsToNumbers = map[int]int{}
+	allMappings             []map[rune]rune
 )
 
+func encode(s string, mapping map[rune]rune) string {
+	return strings.Map(func(r rune) rune {
+		return mapping[r]
+	}, s)
+}
+
+func decode(inputs []string, outputs []string) ([]string, []string) {
+	var encodings map[string]int
+	for _, mapping := range allMappings {
+		expectedEncodings := make(map[string]int, len(numberToSegments))
+		for k, v := range numberToSegments {
+			expectedEncodings[text.SortString(encode(v, mapping))] = k
+		}
+
+		found := true
+		for _, encdig := range append(inputs, outputs...) {
+			if _, ok := expectedEncodings[text.SortString(encdig)]; !ok {
+				found = false
+				break
+			}
+		}
+
+		if found {
+			encodings = expectedEncodings
+			break
+		}
+	}
+
+	if encodings == nil {
+		panic(fmt.Errorf("unable to solve decoding for %v | %v", inputs, outputs))
+	}
+
+	decodedInputs := make([]string, len(inputs))
+	for i, in := range inputs {
+		decodedInputs[i] = numberToSegments[encodings[text.SortString(in)]]
+	}
+	decodedOutputs := make([]string, len(outputs))
+	for i, out := range outputs {
+		decodedOutputs[i] = numberToSegments[encodings[text.SortString(out)]]
+	}
+
+	return decodedInputs, decodedOutputs
+}
+
 func init() {
+	segmentsToNumbers = make(map[string]int, len(numberToSegments))
+	for k, v := range numberToSegments {
+		segmentsToNumbers[v] = k
+	}
+
 	var dupes []int
 	for k, v := range numberToSegments {
 		key := len(v)
@@ -37,6 +90,16 @@ func init() {
 	for _, dupe := range dupes {
 		delete(uniqueSegmentsToNumbers, dupe)
 	}
+
+	letters := "abcdefg"
+	for _, permutation := range text.Permutations(letters) {
+		mapping := make(map[rune]rune, 10)
+		for i, r := range letters {
+			mapping[r] = rune(permutation[i])
+		}
+
+		allMappings = append(allMappings, mapping)
+	}
 }
 
 func main() {
@@ -46,7 +109,7 @@ func main() {
 	data, err := cli.GetInput(*path)
 	cli.ExitOnError(err)
 
-	var part1Count int
+	var part1Count, part2Sum int
 
 	// NOTE: each line has a different encoding, which was very confusing to me
 	// at first.
@@ -63,13 +126,23 @@ func main() {
 		inputs := strings.TrimSpace(parts[0])
 		outputs := strings.TrimSpace(parts[1])
 
-		_ = inputs // not needed for part1
 		for _, encDigit := range strings.Split(outputs, " ") {
 			if _, ok := uniqueSegmentsToNumbers[len(encDigit)]; ok {
 				part1Count++
 			}
 		}
+
+		var buf strings.Builder
+		_, decodedOutputs := decode(strings.Split(inputs, " "), strings.Split(outputs, " "))
+		for _, output := range decodedOutputs {
+			fmt.Fprintf(&buf, "%d", segmentsToNumbers[output])
+		}
+
+		x, err := strconv.ParseInt(buf.String(), 10, 64)
+		cli.ExitOnErrorf(err, "could not parse decoded output %s (line:%d)", buf.String(), i)
+		part2Sum += int(x)
 	}
 
 	fmt.Println(part1Count)
+	fmt.Println(part2Sum)
 }
