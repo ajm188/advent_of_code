1627469fdc3faba2073348c51c263496ec51b282
Andrew Mason <amason@hey.com>
2021-12-09 08:23:29 -0500

    day9, part2

--- 2021/day09/main.go
+++ 2021/day09/main.go
@@ -10,9 +10,16 @@ import (
 	"github.com/ajm188/advent_of_code/pkg/cli"
 )
 
+type Coordinate struct{ Row, Col int }
+
+func (c *Coordinate) String() string { return fmt.Sprintf("(%d, %d)", c.Row, c.Col) }
+
 type Point struct {
 	Height    int
 	Neighbors []int
+
+	Coordinate          *Coordinate
+	NeighborCoordinates []*Coordinate
 }
 
 func (p *Point) IsLow() bool {
@@ -25,26 +32,33 @@ func (p *Point) Risk() int {
 
 func NewPoint(i, j int, heightmap [][]int) *Point {
 	neighbors := make([]int, 0, 4)
+	coordinates := make([]*Coordinate, 0, 4)
 	if i > 0 {
 		neighbors = append(neighbors, heightmap[i-1][j])
+		coordinates = append(coordinates, &Coordinate{Row: i - 1, Col: j})
 	}
 
 	if j > 0 {
 		neighbors = append(neighbors, heightmap[i][j-1])
+		coordinates = append(coordinates, &Coordinate{Row: i, Col: j - 1})
 	}
 
 	if i < len(heightmap)-1 {
 		neighbors = append(neighbors, heightmap[i+1][j])
+		coordinates = append(coordinates, &Coordinate{Row: i + 1, Col: j})
 	}
 
 	if j < len(heightmap[i])-1 {
 		neighbors = append(neighbors, heightmap[i][j+1])
+		coordinates = append(coordinates, &Coordinate{Row: i, Col: j + 1})
 	}
 
 	sort.Ints(neighbors)
 	return &Point{
-		Height:    heightmap[i][j],
-		Neighbors: neighbors,
+		Height:              heightmap[i][j],
+		Neighbors:           neighbors,
+		Coordinate:          &Coordinate{Row: i, Col: j},
+		NeighborCoordinates: coordinates,
 	}
 }
 
@@ -96,19 +110,64 @@ func main() {
 		cli.ExitOnError(fmt.Errorf("rows do not all have same length: %s", buf.String()))
 	}
 
-	var points []*Point
-	for i := 0; i < len(heightmap); i++ {
-		for j := 0; j < len(heightmap[i]); j++ {
-			points = append(points, NewPoint(i, j, heightmap))
-		}
-	}
+	var (
+		// part1
+		totalRisk int
 
-	var totalRisk int
-	for _, p := range points {
-		if p.IsLow() {
-			totalRisk += p.Risk()
+		// part2
+		points    = make([][]*Point, len(heightmap))
+		lowPoints []*Point
+	)
+	for i := 0; i < len(heightmap); i++ {
+		row := make([]*Point, len(heightmap[i]))
+
+		for j := 0; j < len(heightmap[i]); j++ {
+			p := NewPoint(i, j, heightmap)
+			row[j] = p
+			if p.IsLow() {
+				lowPoints = append(lowPoints, p)
+				totalRisk += p.Risk()
+			}
 		}
+
+		points[i] = row
 	}
 
 	fmt.Println(totalRisk)
+
+	var basinSizes []int
+	for _, center := range lowPoints {
+		var (
+			size int
+			seen = map[string]struct{}{
+				center.Coordinate.String(): {},
+			}
+			current = center
+			next    []*Point
+		)
+
+		for next /*covers first iteration */ == nil || len(next) != 0 {
+			if current.Height == 9 {
+				goto iter
+			}
+
+			for _, coord := range current.NeighborCoordinates {
+				if _, ok := seen[coord.String()]; ok {
+					continue
+				}
+
+				next = append(next, points[coord.Row][coord.Col])
+				seen[coord.String()] = struct{}{}
+			}
+			size++
+
+		iter:
+			current, next = next[0], next[1:]
+		}
+
+		basinSizes = append(basinSizes, size)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(basinSizes)))
+	fmt.Println(basinSizes[0] * basinSizes[1] * basinSizes[2])
 }
