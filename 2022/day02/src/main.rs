01742c957c0e5389dfbfd26cef7a4db463923342
Andrew Mason <amason@hey.com>
2022-12-04 10:03:00 -0500

    day2, part 2
    Signed-off-by: Andrew Mason <amason@hey.com>

--- 2022/day02/src/main.rs
+++ 2022/day02/src/main.rs
@@ -12,11 +12,40 @@ enum MatchResult {
 }
 
 impl MatchResult {
+    fn parse(raw: &str) -> Result<MatchResult, String> {
+        match raw {
+            "X" => Ok(Self::LOSE),
+            "Y" => Ok(Self::DRAW),
+            "Z" => Ok(Self::WIN),
+            _ => Err(format!("unknown input: {:?}", raw)),
+        }
+    }
+    
     fn score(self) -> i64 {
         match self {
             Self::WIN => 6,
             Self::LOSE => 0,
             Self::DRAW => 3,
+        }
+    }
+
+    fn needed_throw(self, opponent: Move) -> Move {
+        match self {
+            Self::WIN => {
+                match opponent {
+                    Move::ROCK => Move::PAPER,
+                    Move::PAPER => Move::SCISSORS,
+                    Move::SCISSORS => Move::ROCK,
+                }
+            },
+            Self::LOSE => {
+                match opponent {
+                    Move::ROCK => Move::SCISSORS,
+                    Move::PAPER => Move::ROCK,
+                    Move::SCISSORS => Move::PAPER,
+                }
+            },
+            Self::DRAW => opponent,
         }
     }
 }
@@ -86,6 +115,24 @@ impl Round {
     }
 }
 
+struct Part2Round {
+    opponent: Move,
+    outcome: MatchResult,
+}
+
+impl Part2Round {
+    fn new(opponent: Move, outcome: MatchResult) -> Part2Round {
+        Part2Round{
+            opponent: opponent,
+            outcome: outcome,
+        }
+    }
+
+    fn as_round(self) -> Round {
+        Round { opponent: self.opponent, response: self.outcome.needed_throw(self.opponent) }
+    }
+}
+
 fn main() {
     let reader: Box<dyn BufRead> = io::new_reader(env::args().nth(1));
     let round_re = Regex::new("([ABCXYZ]) ([ABCXYZ])").unwrap();
@@ -94,7 +141,13 @@ fn main() {
     let rounds = round_re.captures_iter(&text).map(|cap| {
         Round::new(Move::parse(&cap[1]).unwrap(), Move::parse(&cap[2]).unwrap())
     });
+    let part2_rounds = round_re.captures_iter(&text).map(|cap| {
+        Part2Round::new(Move::parse(&cap[1]).unwrap(), MatchResult::parse(&cap[2]).unwrap())
+    });
 
     let advised_score = rounds.map(|r| r.score()).sum::<i64>();
     println!("part 1: {:?}", advised_score);
+    let part2_advised_score = part2_rounds.map(|r| r.as_round().score()).sum::<i64>();
+    println!("part 2: {:?}", part2_advised_score);
+
 }
