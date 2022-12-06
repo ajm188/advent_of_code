2e9effda52ec3a525808f2159b622d3cf4d92a63
Andrew Mason <amason@hey.com>
2022-12-05 20:21:02 -0500

    day3, part2
    Signed-off-by: Andrew Mason <amason@hey.com>

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -2,6 +2,8 @@ use std::collections::HashSet;
 use std::env;
 use std::io::BufRead;
 
+use itertools::Itertools;
+
 use lib::io;
 use lib::set;
 
@@ -17,10 +19,9 @@ fn item_priority(c: char) -> i32 {
 
 fn main() {
     let reader: Box<dyn BufRead> = io::new_reader(env::args().nth(1));
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap() ).collect();
 
-    let halves = reader.lines().map(|line| 
-        line.unwrap()
-    ).map(|line| -> (Vec<char>, Vec<char>) {
+    let halves = (&lines).into_iter().map(|line| -> (Vec<char>, Vec<char>) {
         (line.chars().into_iter().take(line.len() / 2).collect(), line.chars().into_iter().skip(line.len() / 2).collect())
     });
 
@@ -38,4 +39,17 @@ fn main() {
     let part1 = commons.map(item_priority).sum::<i32>();
     
     println!("part1: {:?}", part1);
+
+    let part2 = (&lines).into_iter().map(|line| line.chars()).tuples().map(|(x, y, z)| -> Vec<HashSet<char>> {
+        vec![
+            HashSet::from_iter(x),
+            HashSet::from_iter(y),
+            HashSet::from_iter(z),
+        ]
+    }).into_iter().map(|sets| {
+        let items: Vec<&char> = set::intersect_all(&sets).into_iter().collect();
+        *items[0]
+    }).map(item_priority).sum::<i32>();
+
+    println!("part2: {:?}", part2);
 }
